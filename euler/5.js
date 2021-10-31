52fa632529e2febbfa838e4f357d3c2909d0a806
Bilal ATLI <ytbilalatli@gmail.com>
2021-10-31 22:47:06 +0300

    Solved 5th problem

---
+++ euler/5.js
@@ -0,0 +1,7 @@
+const leastCommonMultiple = require('./../utility/least-common-multiple');
+
+const solve = () => {
+    return leastCommonMultiple(1, 20);
+};
+
+console.log(`#5 Result : ${solve()}`);
